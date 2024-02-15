ce36055463783b54789d0a065c08ed273fa5b6f2
christophertesar <ctesar@sfu.ca>
2024-02-14 18:14:44 -0800

    Added adaptive widget sizes depending on screen size
    Added svg icons for social media
    Added widget for social media

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyPortfolioApp());
@@ -25,11 +26,21 @@ class PortfolioPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      padding: const EdgeInsets.all(50),
-      children: const <Widget>[
+      padding: EdgeInsets.all(
+        context.responsive<double>(
+          20, // default
+          sm: 10, // small 
+          md: 20, // medium
+          lg: 30, // large 
+          xl: 50, // extra large screen
+        ),
+      ),
+      children: <Widget>[
         TitlePage(),
         // Each child represents a page in the portfolio
         ExperiencePage(),
+
+        ContactsPage(),
         // Add more PortfolioItem widgets for additional projects
       ],
     );
@@ -42,6 +53,7 @@ class TitlePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+
     return Container(
       alignment: Alignment.topLeft,
       child: Column(
@@ -51,21 +63,50 @@ class TitlePage extends StatelessWidget{
             duration: Duration(seconds: 1),
             opacity: 1.0,
             child: RichText(
-              text: const TextSpan(
+              text: TextSpan(
                 text: "hi,",
-                style: TextStyle(fontFamily: "Aileron", fontWeight: FontWeight.w400, fontSize: 100, color: Color.fromARGB(255, 248, 238, 224)),
+                style: TextStyle(
+                  fontFamily: "Aileron", 
+                  fontWeight: FontWeight.w400, 
+                  fontSize: context.responsive<double>(
+                    72, // default
+                    sm: 50, // small 
+                    md: 72, // medium
+                    lg: 86, // large 
+                    xl: 100, // extra large screen
+                  ), 
+                  color: Color.fromARGB(255, 248, 238, 224)),
                 children: <TextSpan>[
                   TextSpan(
                       text: ' nice to meet you. I\'m',
-                      style: TextStyle(fontFamily: "Aileron", fontWeight: FontWeight.w200, fontSize: 100, color: Color.fromARGB(255, 248, 238, 224))
+                      style: TextStyle(
+                        fontFamily: "Aileron",
+                        fontWeight: FontWeight.w200,
+                        fontSize: context.responsive<double>(
+                          72, // default
+                          sm: 50, // small 
+                          md: 72, // medium
+                          lg: 86, // large 
+                          xl: 100, // extra large screen
+                        ), 
+                        color: Color.fromARGB(255, 248, 238, 224))
                   ),
                 ]
               )
             ),
           ),
-          const Text(
+          Text(
             textAlign: TextAlign.left,
-            style: TextStyle(fontFamily: "Aileron", fontWeight: FontWeight.w800, fontSize: 132, color: Color.fromARGB(255, 248, 238, 224)),
+            style: TextStyle(fontFamily: "Aileron", 
+            fontWeight: FontWeight.w800, 
+            fontSize: context.responsive<double>(
+              86, // default
+              sm: 72, // small 
+              md: 86, // medium
+              lg: 100, // large 
+              xl: 132, // extra large screen, 
+            ),
+            color: Color.fromARGB(255, 248, 238, 224)),
             "Christopher",
           ),
           const Padding(
@@ -85,8 +126,20 @@ class TitlePage extends StatelessWidget{
                 ),
                 child: Image.asset(
                   'assets/self.png', // Replace with your image URL
-                  width: 800, // Adjust width as needed
-                  height: 800, // Adjust height as needed
+                  width: context.responsive<double>(
+                    400, // default
+                    sm: 200, // small 
+                    md: 400, // medium
+                    lg: 600, // large 
+                    xl: 800, // extra large screen
+                  ), // Adjust width as needed
+                  height: context.responsive<double>(
+                    400, // default
+                    sm: 200, // small 
+                    md: 400, // medium
+                    lg: 600, // large 
+                    xl: 800, // extra large screen
+                  ), // Adjust height as needed
                   fit: BoxFit.cover, // Adjust image fitting as needed
                 ),
               ),
@@ -96,27 +149,61 @@ class TitlePage extends StatelessWidget{
             ),
             Container(
               alignment: Alignment.topLeft,
-              width: 1200,
-              child: const Column(
+              width: context.responsive<double>(
+                600, // default
+                sm: 400, // small 
+                md: 600, // medium
+                lg: 900, // large 
+                xl: 1200, // extra large screen
+              ),
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                   textAlign: TextAlign.left,
                   "I'm a software developer.",
-                  style: TextStyle(fontFamily: "Aileron", fontWeight: FontWeight.w200, fontSize: 100, color: Color.fromARGB(255, 248, 238, 224)),
+                  style: TextStyle(
+                    fontFamily: "Aileron", 
+                    fontWeight: FontWeight.w200, 
+                    fontSize: context.responsive<double>(
+                      50, // default
+                      sm: 36, // small 
+                      md: 50, // medium
+                      lg: 72, // large 
+                      xl: 86, // extra large screen
+                    ), 
+                    color: Color.fromARGB(255, 248, 238, 224)),
                   ),
                   Text(
                     textAlign: TextAlign.left,
                     "I love to build meaningful projects.",
-                    style: TextStyle(fontFamily: "Aileron", fontWeight: FontWeight.w200, fontSize: 100, color: Color.fromARGB(255, 248, 238, 224)),
+                    style: TextStyle(
+                    fontFamily: "Aileron", 
+                    fontWeight: FontWeight.w200, 
+                    fontSize: context.responsive<double>(
+                      50, // default
+                      sm: 36, // small 
+                      md: 50, // medium
+                      lg: 72, // large 
+                      xl: 86, // extra large screen
+                    ), 
+                    color: Color.fromARGB(255, 248, 238, 224)),
                   ),
                 ],
               )
             ),
             ],
           ),
-          const Padding(
-            padding: EdgeInsets.only(top: 300.0),
+          Padding(
+            padding: EdgeInsets.only(
+              top: context.responsive<double>(
+                100, // default
+                sm: 50, // small 
+                md: 100, // medium
+                lg: 200, // large 
+                xl: 300, // extra large screen
+              )
+            ),
           ) 
         ],
       )
@@ -129,16 +216,32 @@ class ExperiencePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-            textAlign: TextAlign.left,
-            "Experience",
-            style: TextStyle(fontFamily: "Aileron", fontWeight: FontWeight.w800, fontSize: 100, color: Color.fromARGB(255, 248, 238, 224)),
-          ),
+          textAlign: TextAlign.left,
+          "Experience",
+          style: TextStyle(
+            fontFamily: "Aileron", 
+            fontWeight: FontWeight.w800, 
+            fontSize: context.responsive<double>(
+              72, // default
+              sm: 50, // small 
+              md: 72, // medium
+              lg: 86, // large 
+              xl: 100, // extra large screen
+            ), 
+            color: Color.fromARGB(255, 248, 238, 224)),
+        ),
         Padding(
-          padding: EdgeInsets.only(top: 50.0),
+          padding: EdgeInsets.only(top: context.responsive<double>(
+              30, // default
+              sm: 20, // small 
+              md: 30, // medium
+              lg: 40, // large 
+              xl: 50, // extra large screen
+          )),
         ),
         ExperienceItem(
           logoAssetPath: 'assets/LMI_logo.jpg',
@@ -164,7 +267,6 @@ class ExperiencePage extends StatelessWidget{
             "Created PCB footprints using KiCAD to create a custom external ADC board for antenna data acquisition.",
           ],
         )
-
       ],
     );
   }
@@ -187,8 +289,20 @@ class ExperienceItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 600,
-      width: 1920,
+      height: context.responsive<double>(
+        1024, // default
+        sm: 450, // small 
+        md: 500, // medium
+        lg: 550, // large 
+        xl: 600, // extra large screen
+      ),
+      width: context.responsive<double>(
+        1024, // default
+        sm: 768, // small 
+        md: 1024, // medium
+        lg: 1081, // large 
+        xl: 1440, // extra large screen
+      ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -203,45 +317,91 @@ class ExperienceItem extends StatelessWidget {
               ),
               child: Image.asset(
                 logoAssetPath,
-                width: 300, // Adjust width as needed
-                height: 300, // Adjust height as needed
+                width: context.responsive<double>(
+                  200, // default
+                  sm: 100, // small 
+                  md: 200, // medium
+                  lg: 250, // large 
+                  xl: 300, // extra large screen
+                ), // Adjust width as needed
+                height: context.responsive<double>(
+                  200, // default
+                  sm: 100, // small 
+                  md: 200, // medium
+                  lg: 250, // large 
+                  xl: 300, // extra large screen
+                ), // Adjust height as needed
                 fit: BoxFit.cover, // Adjust image fitting as needed
               ),
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.only(left: 50.0),
+          Padding(
+            padding: EdgeInsets.only(
+              left: context.responsive<double>(
+                30, // default
+                sm: 20, // small 
+                md: 30, // medium
+                lg: 40, // large 
+                xl: 50, // extra large screen
+              ),
+            ),
           ),
           Container(
             alignment: Alignment.topLeft,
-            width: 1200,
+            width: context.responsive<double>(             
+                750, // default
+                sm: 550, // small 
+                md: 750, // medium
+                lg: 950, // large 
+                xl: 1200, // extra large screen
+              ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 RichText(
                   text: TextSpan(
                     text: positionName,
-                    style: const TextStyle(
+                    style: TextStyle(
                       fontFamily: "Aileron",
                       fontWeight: FontWeight.w600,
-                      fontSize: 36,
+                      fontSize: context.responsive<double>(
+                        28, // default
+                        sm: 24, // small 
+                        md: 28, // medium
+                        lg: 32, // large 
+                        xl: 36, // extra large screen
+                      ),
                       color: Color.fromARGB(255, 248, 238, 224),
                     ),
                     children: <TextSpan>[
                       TextSpan(
                         text: ' @ $companyName',
-                        style: const TextStyle(
+                        style: TextStyle(
                           fontFamily: "Aileron",
                           fontWeight: FontWeight.w200,
-                          fontSize: 36,
+                          fontSize: context.responsive<double>(
+                            28, // default
+                            sm: 24, // small 
+                            md: 28, // medium
+                            lg: 32, // large 
+                            xl: 36, // extra large screen
+                          ),
                           color: Color.fromARGB(255, 248, 238, 224),
                         ),
                       ),
                     ],
                   ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 50.0),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: context.responsive<double>(
+                      30, // default
+                      sm: 20, // small 
+                      md: 30, // medium
+                      lg: 40, // large 
+                      xl: 50, // extra large screen
+                    ),
+                  ),
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -250,10 +410,16 @@ class ExperienceItem extends StatelessWidget {
                         (point) => Text(
                           '• $point',
                           textAlign: TextAlign.left,
-                          style: const TextStyle(
+                          style: TextStyle(
                             fontFamily: "Aileron",
                             fontWeight: FontWeight.w200,
-                            fontSize: 24,
+                            fontSize: context.responsive<double>(
+                              20, // default
+                              sm: 18, // small 
+                              md: 20, // medium
+                              lg: 22, // large 
+                              xl: 24, // extra large screen
+                            ),
                             color: Color.fromARGB(255, 248, 238, 224),
                           ),
                         ),
@@ -269,3 +435,67 @@ class ExperienceItem extends StatelessWidget {
   }
 }
 
+class ContactsPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return GridView.count(
+      shrinkWrap: true, // Make the GridView non-scrollable
+      crossAxisCount: 3, // Number of columns
+      children: <Widget>[
+        Text('Text 1'),
+        Text('Text 2'),
+        Text('Text 3'),
+        Text('Text 4'),
+        Text('Text 5'),
+        Text('Text 6'),
+      ],
+    );
+    }
+}
+
+class SocialItem extends StatelessWidget{
+  final String logoAssetPath;
+  final String text;
+
+  const SocialItem({
+    Key? key,
+    required this.logoAssetPath,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Row(
+      children: [
+        Image.asset(
+          logoAssetPath, // Replace with your image URL
+          width: 25, // Adjust width as needed
+          height: 25, // Adjust height as needed
+          fit: BoxFit.cover, // Adjust image fitting as needed
+        ),
+        Text(text)
+      ],
+    );
+  }
+}
+
+extension Responsive on BuildContext {
+  T responsive<T>(
+    T defaultVal, {
+    T? sm,
+    T? md,
+    T? lg,
+    T? xl,
+  }) {
+    final wd = MediaQuery.of(this).size.width;
+    return wd >= 1440
+        ? (xl ?? lg ?? md ?? sm ?? defaultVal)
+        : wd >= 1281
+            ? (lg ?? md ?? sm ?? defaultVal)
+            : wd >= 1024
+                ? (md ?? sm ?? defaultVal)
+                : wd >= 768
+                    ? (sm ?? defaultVal)
+                    : defaultVal;
+  }
+}
